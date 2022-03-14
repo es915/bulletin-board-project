863f3b4c269ae6f80dd78ed32154b21b3c9f9ce2
eulseon <95990910+es915@users.noreply.github.com>
2022-03-14 22:05:45 +0900

    Implement update function

--- Main.java
+++ Main.java
@@ -6,11 +6,30 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+	
+		Board board = new Board();
+		board.run();
 		
-		// 게시물 제목들만 담아둔 리스트
-		ArrayList<String> titles = new ArrayList<>();
-		// 게시물 내용들만 담아둔 리스트
-		ArrayList<String> bodies = new ArrayList<>();
+	}
+}
+
+class Board {
+	
+	// 게시물 제목들만 담아둔 리스트
+	ArrayList<String> titles = new ArrayList<>();
+	// 게시물 내용들만 담아둔 리스트
+	ArrayList<String> bodies = new ArrayList<>();
+
+	public void printArticles() {
+		for(int i=0; i<titles.size(); i++) {
+			System.out.printf("번호 : %d\n", i+1);
+			System.out.printf("제목 : %s\n", titles.get(i));
+			System.out.printf("내용 : %s\n", bodies.get(i));
+			System.out.println("=========================");
+		}
+	}
+	
+	public void run() {
 		Scanner sc = new Scanner(System.in);
 		
 		// 명령어를 계속 받기위한 while 
@@ -38,15 +57,29 @@ public class Main {
 				// 게시물 리스트 출력 명령어
 			} else if(command.equals("list")) {
 				
-				for(int i=0; i<titles.size(); i++) {
-					System.out.printf("번호 : %d\n", i+1);
-					System.out.printf("제목 : %s\n", titles.get(i));
-					System.out.printf("내용 : %s\n", bodies.get(i));
-					System.out.println("=========================");
+				printArticles();
+				
+			} else if(command.equals("update")) {
+				System.out.println("수정할 게시물 번호 : ");
+				int no = Integer.parseInt(sc.nextLine())-1;
+				
+				if(no < 0 || no >= titles.size()) {
+					System.out.println("없는 게시물입니다.");
+				} else {
+					System.out.println("새 제목을 입력해주세요 : ");
+					String newTitle = sc.next();
+					System.out.println("새 내용을 입력해주세요 : ");
+					String newBody = sc.next();
+					
+					titles.set(no, newTitle);
+					bodies.set(no, newBody);
+					System.out.println("수정이 완료되었습니다\n");
+					
+					printArticles();
+					
 				}
 				
-			}	
-		}		
+			}
+		}	
 	}
-
 }
