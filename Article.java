641aa3ce00c7425de6ea90e88f47c1772737838c
eulseon <95990910+es915@users.noreply.github.com>
2022-03-22 23:28:39 +0900

    file refactoring

---
+++ Article.java
@@ -0,0 +1,42 @@
+package board;
+
+class Article {
+	private int idx;
+	private String title;
+	private String body;
+	
+	public Article() {
+		
+	}
+
+	public Article(int idx, String title, String body) {
+		super();
+		this.idx = idx;
+		this.title = title;
+		this.body = body;
+	}
+
+	public int getIdx() {
+		return idx;
+	}
+
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+}
